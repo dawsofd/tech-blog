cc8522ba8dc98aad2e70485622aa7cf0aa773c3d
Dawson Dohlen <d.dohlen@pm.me>
2024-11-08 22:33:42 -0800

    add handlebar file templates and update dependencies in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +21,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
