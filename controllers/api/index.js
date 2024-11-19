9547eb221e905dc02a47d1f891e58c2a4350e151
Dawson Dohlen <d.dohlen@pm.me>
2024-11-18 23:24:53 -0800

    edit login.js function

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
-// Import the routes. This is how we make our routes modular.
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-// When a request is made to the /users, /posts, or /commments path, it will be directed to the index.js in the /users, /posts, or /comments folder.
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
